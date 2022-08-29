c155c8c0d86431945a54925bef1a4a059fb4cc56
Jiya Bhagat <97676121+juniperus01@users.noreply.github.com>
2022-08-29 15:44:50 +0530

    Fixed bugs

--- main.py
+++ main.py
@@ -31,10 +31,10 @@ def clear():
 
 screen.listen()
 screen.onkey(key="w", fun=move_forwards)
-screen.onkey(key="s", fun=move_backwards())
-screen.onkey(key="a", fun=turn_left())
-screen.onkey(key="d", fun=turn_right())
-screen.onkey(key="space", fun=clear())
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="space", fun=clear)
 
 
 screen.exitonclick()
